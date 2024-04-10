f4d6d8cbb51a8a126ad84c49e9be5afff89015fc
Dennis Lammers <dennislammers@live.nl>
2024-04-10 13:48:03 +0200

    chore: Satis

--- satis/docker-entrypoint.sh
+++ satis/docker-entrypoint.sh
@@ -3,8 +3,13 @@
 # additional SSH keys provided via volume
 if [ -d /var/www/.ssh-keys ]; 
 then
+    NUMFILES=$(ls -1 | wc -l)
+    echo "Found $NUMFILES additional SSH keys"
     cp -r /var/www/.ssh-keys/* /root/.ssh/;
     chmod 600 /root/.ssh/*
+    echo "Copied $NUMFILES additional SSH keys"
+else 
+    echo "Found no additional SSH keys"
 fi
 
 sh /satis/bin/docker-entrypoint.sh
